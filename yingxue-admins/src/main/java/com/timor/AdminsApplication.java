45aac76e24e8082c398e32b763ecf639c8d16db5
TimorAchor <150976977@qq.com>
2021-09-15 00:18:42 +0800

    first commit

---
+++ yingxue-admins/src/main/java/com/timor/AdminsApplication.java
@@ -0,0 +1,11 @@
+package com.timor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminsApplication.class,args);
+    }
+}
